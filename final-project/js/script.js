be2c73de87fb79c3377cb40814a39c44e5103a3e
Liang-Yu Liu <lyl23001@uconn.edu>
2026-04-28 11:05:35 -0400

    6

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,1 +1,7 @@
-console.log("JavaScript loaded successfully.");
+console.log("Page loaded");
+
+document.querySelectorAll(".start-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Started!");
+  });
+});
